e3cdca333be692c91084675db3cc60954c8bf42c
Subhadip Sarkar <edusubhadip@gmail.com>
2023-09-10 21:10:33 +0530

    added gave over logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -16,11 +16,17 @@ var checkValue = function () {
     document.querySelector('.number').textContent = number;
     document.body.style.background = 'lightgreen';
     document.querySelector('.message').textContent = 'Congratulations!!!!🎉🎉';
+    document.querySelector('.highscore').textContent = highScore;
   } else {
     score--;
     document.querySelector('.message').textContent =
       guess > number ? 'Too high. Try again!' : 'Too low. Try again!';
     document.querySelector('.score').textContent = score;
+    if (score === 0) {
+      document.querySelector('.message').textContent =
+        'Game Over!!❌❌ Please Try Again';
+      document.querySelector('.btn.check').disabled = true;
+    }
   }
 };
 
